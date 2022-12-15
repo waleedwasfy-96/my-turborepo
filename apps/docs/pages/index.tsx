50fe99cc0ee85606579d67be2cabf7c577dd2dae
waleedwasfy-96 <wasfymow@amazon.com>
2022-12-15 12:15:36 +0000

    add SSR function

--- apps/docs/pages/index.tsx
+++ apps/docs/pages/index.tsx
@@ -1,5 +1,22 @@
 import { Button } from "ui";
 
+interface House {
+  name: string,
+  region: string,
+  url: string
+}
+
+export async function getServerSideProps() {
+  // Fetch data from external API
+  const res = await fetch(`https://www.anapioficeandfire.com/api/houses`)
+  const data: House[] = await res.json();
+
+  console.log("Test some logging!");
+
+  // Pass data to the page via props
+  return { props: { data } }
+}
+
 export default function Docs() {
   return (
     <div>
